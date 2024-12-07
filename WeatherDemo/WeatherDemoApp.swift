ba616253ce7dd3275ef09aedf78402b9037599dd
Krystian Wiewior <krystian.wiewior@gmail.com>
2024-12-07 18:06:58 +0100

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Krystian Wiewiór on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
