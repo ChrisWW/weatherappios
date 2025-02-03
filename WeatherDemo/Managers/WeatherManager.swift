5fce2740f86a4b0b3778b28c3b51e5004927c36f
Krystian Wiewior <krystian.wiewior@gmail.com>
2025-02-03 01:03:51 +0100

    Use getcurrentweather in contentview

--- WeatherDemo/Managers/WeatherManager.swift
+++ WeatherDemo/Managers/WeatherManager.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
-    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(Secrets.apiKey)&units=metric") else { fatalError("Missing URL")}
         
         let urlRequest = URLRequest(url: url)
@@ -17,5 +17,75 @@ class WeatherManager {
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         
         guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data") }
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
     }
+}
+
+struct ResponseBody: Decodable {
+    let coord: Coordinates
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let rain: Rain?
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone: Int
+    let id: Int
+    let name: String
+    let cod: Int
+}
+
+struct Coordinates: Decodable {
+    let lon: Double
+    let lat: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let feels_like: Double
+    let temp_min: Double
+    let temp_max: Double
+    let pressure: Int
+    let humidity: Int
+    let sea_level: Int?
+    let grnd_level: Int?
+}
+
+struct Wind: Decodable {
+    let speed: Double
+    let deg: Int
+    let gust: Double?
+}
+
+struct Rain: Decodable {
+    let oneHour: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case oneHour = "1h"
+    }
+}
+
+struct Clouds: Decodable {
+    let all: Int
+}
+
+struct Sys: Decodable {
+    let type: Int?
+    let id: Int?
+    let country: String
+    let sunrise: Int
+    let sunset: Int
 }
